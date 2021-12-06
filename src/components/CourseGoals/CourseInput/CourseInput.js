feffeb70c44139e50e80dca99206fd0c84847750
morning-codexx <danitoik028@gmail.com>
2021-12-06 03:51:49 -0800

    todo-list

---
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+
+import Button from '../../UI/Button/Button';
+import './CourseInput.css';
+
+const CourseInput = props => {
+  const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true);
+
+  const goalInputChangeHandler = event => {
+
+    if(event.target.value.trim().length > 0){ 
+
+      setIsValid(true)
+
+    }
+
+     
+    setEnteredValue(event.target.value);
+    // console.log(event.target.value);
+    
+  };
+
+  const formSubmitHandler = event => {
+    // event.preventDefault();
+    event.preventDefault();
+    if(enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
+    props.onAddGoal(enteredValue);
+    setEnteredValue('');
+  };
+
+  return (
+    <form onSubmit={formSubmitHandler}  >
+
+      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+
+        <label>Course Goal</label>
+
+        <input type="text" value={enteredValue} onChange={goalInputChangeHandler} />
+
+      </div>
+      <Button type="submit">Add Goal</Button>
+
+    </form>
+  );
+};
+
+export default CourseInput;
